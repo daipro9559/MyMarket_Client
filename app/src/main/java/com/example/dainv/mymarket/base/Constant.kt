fa9b9d8fb12d901da80999ac8350145c6e44e158
Dai Nguyen <daipro9559@gmail.com>
2018-10-10 07:51:59 +0700

    .

--- app/src/main/java/com/example/dainv/mymarket/base/Constant.kt
+++ app/src/main/java/com/example/dainv/mymarket/base/Constant.kt
@@ -3,7 +3,7 @@ package com.example.dainv.mymarket.base
 
 object Constant {
     val IS_DEBUG = true
-    val BASE_URL = "http://192.168.1.102:3000/v1/"
+    val BASE_URL = "http://192.168.3.115:3000/v1/"
 
     // share Preference key
     val TOKEN = "token bear"
