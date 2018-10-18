6817be7394b2520eb40c05068d4249c09f3e0fad
Dai Nguyen <daipro9559@gmail.com>
2018-10-18 08:03:01 +0700

    .

---
+++ app/src/main/java/com/example/dainv/mymarket/ui/main/profile/ProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.example.dainv.mymarket.ui.main.profile
+
+import com.example.dainv.mymarket.base.BaseFragment
+
+class ProfileFragment :BaseFragment() {
+    override fun getLayoutID(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
