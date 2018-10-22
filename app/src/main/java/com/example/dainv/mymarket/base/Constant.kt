72605b6773c17f3b3e3b83b05eb934e738ed9a3e
Dai Nguyen <daipro9559@gmail.com>
2018-10-22 07:59:44 +0700

    Merge branch 'feature/main' of https://github.com/daipro9559/MyMarket_Client into feature/main

--- app/src/main/java/com/example/dainv/mymarket/base/Constant.kt
+++ app/src/main/java/com/example/dainv/mymarket/base/Constant.kt
@@ -3,7 +3,7 @@ package com.example.dainv.mymarket.base
 
 object Constant {
     const val IS_DEBUG = true
-    const val BASE_URL = "http://192.168.1.102:3000/v1/"
+    const val BASE_URL = "http://192.168.3.110:3000/v1/"
     const val HEADER = "Authorization"
     // share Preference key
     const val TOKEN = "token bear"
